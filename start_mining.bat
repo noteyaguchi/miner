308b705b6f9c6f9c7d96bf70af359696fc757a5f
noteyaguchi <rachavut@gmail.com>
2018-02-16 09:34:13 +0700

    Update start_mining.bat

--- start_mining.bat
+++ start_mining.bat
@@ -1,8 +1,8 @@
 :loop
 for /f "skip=1 delims=" %%A in ('wmic computersystem get name') do for /f "delims=" %%B in ("%%A") do set "RigsNo=%%~nxB"
-::ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EbcCkwZrWGKX9GgHWrKC3tvV6KKzkMCCJR.%RigsNo% -p c=END
-ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4233 -u c22z1dxuj6BvueFBtNJJr8RuKKtTWKxVbD.%RigsNo% -p c=CROP,d=1024 
-::ccminer-alexis78-ms2013-cuda7.5\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EbcCkwZrWGKX9GgHWrKC3tvV6KKzkMCCJR.rig1 -p c=END
+::ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EVtXye3rd79Xbj1uj1RWbwEYk4nZxtUExq.%RigsNo% -p c=END
+ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4233 -u c3MguvHSBuRTLMayziBRZkswKU5ywBKquG.%RigsNo% -p c=CROP,d=1024 
+::ccminer-alexis78-ms2013-cuda7.5\ccminer -a neoscrypt -o stratum+tcp://tiny-pool.com:4234 -u EVtXye3rd79Xbj1uj1RWbwEYk4nZxtUExq.rig1 -p c=END
 
 timeout /T 10
 goto loop
